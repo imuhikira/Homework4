4e6d69d11a059a45d18849c812dd5cc5c794d1eb
muhikira <imuhikira@dmacc.edu>
2019-02-16 21:26:58 -0600

    homework4

---
+++ VariableAdder.java
@@ -0,0 +1,21 @@
+package edu.dmacc.codedsm.hw4;
+
+import sun.security.provider.Sun;
+
+public class VariableAdder {
+
+    final static int GINGERALE= 15;
+    static int sunkist= 10;
+
+
+    public static void main(String[] args) {
+
+        int strawBerry = 5;
+
+        int result = GINGERALE + sunkist + strawBerry;
+        System.out.println(result);
+
+
+
+    }
+}
